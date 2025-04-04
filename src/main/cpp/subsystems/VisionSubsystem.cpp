18f135b68b897d9b4652dbb2cb73dcc8590e0889
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2025-04-04 15:33:38 -0400

    Use closest to camera height fallback strategy for better stability

--- src/main/cpp/subsystems/VisionSubsystem.cpp
+++ src/main/cpp/subsystems/VisionSubsystem.cpp
@@ -2,18 +2,22 @@
 
 using namespace VisionConstants;
 
+constexpr auto kFallbackStrategy = photon::CLOSEST_TO_CAMERA_HEIGHT;
+
 VisionSubsystem::VisionSubsystem(std::function<frc::Pose3d ()> accessor, std::function<void (frc::Pose3d, units::millisecond_t)> updater) :
   m_camera{new photon::PhotonCamera{kName}},
   m_poseEstimator{kAprilTags, photon::MULTI_TAG_PNP_ON_COPROCESSOR, kRobotToCam},
   m_accessor{accessor},
   m_updater{updater}
 {
-  m_poseEstimator.SetMultiTagFallbackStrategy(photon::CLOSEST_TO_REFERENCE_POSE);
+  m_poseEstimator.SetMultiTagFallbackStrategy(kFallbackStrategy);
 }
 
 std::optional<frc::Pose3d> VisionSubsystem::SeedPose() {
   // Switch to a fallback strategy that doesn't depend on knowing an approximate pose
-  m_poseEstimator.SetMultiTagFallbackStrategy(photon::CLOSEST_TO_CAMERA_HEIGHT);
+  if (kFallbackStrategy != photon::CLOSEST_TO_CAMERA_HEIGHT) {
+    m_poseEstimator.SetMultiTagFallbackStrategy(photon::CLOSEST_TO_CAMERA_HEIGHT);
+  }
   std::optional<frc::Pose3d> seed = {};
 
   ProcessCameraResults([&seed](frc::Pose3d pose, units::millisecond_t timestamp) {
@@ -21,7 +25,9 @@ std::optional<frc::Pose3d> VisionSubsystem::SeedPose() {
   });
 
   // Switch back to the original fallback strategy
-  m_poseEstimator.SetMultiTagFallbackStrategy(photon::CLOSEST_TO_REFERENCE_POSE);
+  if (kFallbackStrategy != photon::CLOSEST_TO_CAMERA_HEIGHT) {
+    m_poseEstimator.SetMultiTagFallbackStrategy(kFallbackStrategy);
+  }
 
   return seed;
 }
